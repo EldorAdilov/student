f3e4b0f85a5ae701d50b8c4f96629338f9520208
EldorAdilov <studenteldor62@gmail.com>
2024-02-09 12:07:55 +0500

    Fixed login

--- conf/settings.py
+++ conf/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "jarvis",
-        "USER": "postgres",
-        "PASSWORD": "Eldor1999",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
